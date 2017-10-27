1aadc88017bcca201d33638de0a88449e346e959
Filipe Machado <PoisonOspina@MacBook-Pro-de-Filipe.local>
2017-10-27 17:48:19 -0300

    ajustantes de deploy V3

--- apimaster/deploy.sh
+++ apimaster/deploy.sh
@@ -5,5 +5,5 @@ docker exec nodes sh -c "pip3 install -r /var/www/tretas/requeriments.txt"
 docker exec nodes sh -c "/var/www/tretas/apimaster/manage.py migrate"
 docker exec nodes sh -c "/var/www/tretas/apimaster/manage.py collectstatic --noinput"
 docker exec nodes pkill python3.5
-# docker exec nodes sh -c "/var/www/tretas/apimaster/manage.py runserver 0.0.0.0:9001"
+docker exec nodes sh -c "/var/www/tretas/apimaster/manage.py runserver 0.0.0.0:9001"
 # docker exec -d nodes gunicorn --pythonpath /var/www clubsat.wsgi -b 0.0.0.0:8001
